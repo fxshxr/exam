9088e511f26bf6736c75d497ca95aede2bec664e
fxshxr <igor.lytkin@gmail.com>
2021-01-28 09:53:11 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>admin auth exam lytkin 191-322</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <form action="post">
+        <h1>Добро пожаловать в систему</h1>
+        <p>Введите пароль Администратора (12345)</p>
+        <input type="password" required>
+        <input type="button" value="Войти" >
+        </form>
+    </div>
+</body>
+</html>
