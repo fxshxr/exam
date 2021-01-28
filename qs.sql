abfacba8aba92637468f86d6630168adae16780f
fxshxr <igor.lytkin@gmail.com>
2021-01-28 15:54:17 +0300

    Create qs.sql

---
+++ qs.sql
@@ -0,0 +1,185 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 28, 2021 at 03:53 PM
+-- Server version: 10.3.13-MariaDB-log
+-- PHP Version: 7.1.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `qs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `123ds123ds`
+--
+
+CREATE TABLE `123ds123ds` (
+  `qtext` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `qansw` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `asdasd`
+--
+
+CREATE TABLE `asdasd` (
+  `qtext` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `qansw` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `asdasd`
+--
+
+INSERT INTO `asdasd` (`qtext`, `qansw`) VALUES
+('asdasd', 'asdasd'),
+('asdasd', 'asdasd'),
+('asdasd', 'asdasd'),
+('asdasd', 'asdasd'),
+('asdasd', 'asdasd'),
+('asdasd', 'asdasd'),
+('asdasd', 'asdasd'),
+('asdasd', 'asdasd'),
+('asdasd', 'asdasd'),
+('asdasd', 'asdasd'),
+('asdasd', 'asdasd');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `asdasdd`
+--
+
+CREATE TABLE `asdasdd` (
+  `qtext` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `qansw` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `asdasdd`
+--
+
+INSERT INTO `asdasdd` (`qtext`, `qansw`) VALUES
+('d', 'dfv');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dfsdfssdf`
+--
+
+CREATE TABLE `dfsdfssdf` (
+  `qtext` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `qansw` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `dfsdfssdf`
+--
+
+INSERT INTO `dfsdfssdf` (`qtext`, `qansw`) VALUES
+('dfsdfssdf', 'dfsdfssdf'),
+('dfsdfssdf', 'dfsdfssdf'),
+('dfsdfssdf', 'dfsdfssdf'),
+('dfsdfssdf', 'dfsdfssdf'),
+('dfsdfssdf', 'dfsdfssdf'),
+('dfsdfssdf', 'dfsdfssdf'),
+('dfsdfssdf', 'dfsdfssdf');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sd`
+--
+
+CREATE TABLE `sd` (
+  `qtext` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `qansw` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `sd`
+--
+
+INSERT INTO `sd` (`qtext`, `qansw`) VALUES
+('sd', 'sd'),
+('sd', 'sd'),
+('sd', 'sd');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ssssssssssssss`
+--
+
+CREATE TABLE `ssssssssssssss` (
+  `qtext` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `qansw` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `ssssssssssssss`
+--
+
+INSERT INTO `ssssssssssssss` (`qtext`, `qansw`) VALUES
+('s', 's'),
+('s', 's');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `выц`
+--
+
+CREATE TABLE `выц` (
+  `qtext` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `qansw` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `выц`
+--
+
+INSERT INTO `выц` (`qtext`, `qansw`) VALUES
+('выц', 'выц');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ыв`
+--
+
+CREATE TABLE `ыв` (
+  `qtext` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `qansw` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `ыв`
+--
+
+INSERT INTO `ыв` (`qtext`, `qansw`) VALUES
+('ыв', 'ыв'),
+('ыв', 'ыв');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
