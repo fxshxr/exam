ff4846761ae48c4a7dfd83e533b2a4c6b5d9889f
fxshxr <igor.lytkin@gmail.com>
2021-01-28 15:10:36 +0300

    эээ

--- admin1.php
+++ admin1.php
@@ -2,44 +2,46 @@
  include("sql.php");
  session_start();
     $sess = $_POST['session_id'];
+    $qtext = htmlentities(mysqli_real_escape_string($link, $_POST['qtext']));
+    $qansw = htmlentities(mysqli_real_escape_string($link, $_POST['qansw']));
     $link = mysqli_connect("localhost", "root", "", "qs");
         if (!$link) {
         die("Connection failed: " . mysqli_connect_error());
         }
-        if (isset($_POST['session_id'])){
+        if (isset($_POST['session_id']) && isset($_POST['qtext']) && isset($_POST['qansw'])){
             $sql = "CREATE TABLE $sess (
-            qtype VARCHAR(30) NOT NULL,
             qtext VARCHAR(30) NOT NULL,
             qansw VARCHAR(30) NOT NULL)";
-            $newfile = fopen("$sess.php", "a");
+
+            $sqla = "INSERT INTO $sess values ('$qtext', '$qansw')";
+            
+            mysqli_query($link, $sqla);
+
+            $newfile = fopen("new/$sess.php", "a");
             fwrite($newfile, '
             <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>admin auth exam lytkin 191-322</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <div class="container">
-        <h1>Добро пожаловать в опрос</h1>
-        <form method="post">
-                
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>admin auth exam lytkin 191-322</title>
+                    <link rel="stylesheet" href="styles.css">
+                </head>
+                <body>
+                    <div class="container">
+                        <h1>Добро пожаловать в опрос</h1>   
+                            <p> Вопрос:  </p>');
+            $sqlaa = "select qtext from $sess ";
+            $as = mysqli_query($link, $sqlaa);
+            fwrite($newfile, $as);
             
-        </form>
-        
-    </div>
-</body>
-</html>
+
             
-            ');
-        
             }
 
         if (mysqli_query($link, $sql)) {
             echo "Таблица $sess создана \n\n";
-            echo "<br><a href=$sess.php>ссылка на страницу</a>";
+            echo "<br><a href=new/$sess.php>ссылка на страницу</a>";
            
             } 
         else {
@@ -67,7 +69,9 @@
         <form method="post">
             <h1>Создание новой ссылки</h1>
             <p>Введите название новой ссылки <input name="session_id" type="text" required>  </p>
-           
+            <h1>Создание вопроса</h1>
+            <p>Введите вопрос <input name="qtext" type="text" required>  </p>
+            <p>Введите ответ<input name="qansw" type="text" required>  </p>
             <input type="submit" value="Создать Cсылку">
 
             
---
+++ new/dsv.php
@@ -0,0 +1,13 @@
+
+            <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>admin auth exam lytkin 191-322</title>
+                    <link rel="stylesheet" href="styles.css">
+                </head>
+                <body>
+                    <div class="container">
+                        <h1>Добро пожаловать в опрос</h1>   
+                            <p> Вопрос:  </p>
